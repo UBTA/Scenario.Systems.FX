82ff8ad242c717829d529d3dc956ca5f58b8cbe4
Weackss <62299994+UBTA@users.noreply.github.com>
2024-11-07 19:40:33 +0300

    1.1.0

---
+++ Runtime/FXData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using EblanDev.ScenarioCore.GameFramework.Data;
+using UnityEngine;
+
+namespace EblanDev.ScenarioCore.Systems.FXSystemUnit
+{
+    [CreateAssetMenu(fileName = "FXData", menuName = "ScenarioFramework/Systems/FXData", order = 1)]
+    public class FXData : DataClear
+    {
+        [SerializeField] private List<ParticleInstance> particles;
+
+        public ParticleInstance GetParticle(FXID id)
+        {
+            foreach (var particle in particles)
+            {
+                if (particle.ID == id)
+                {
+                    return particle;
+                }
+            }
+
+            if (particles.Count != 0)
+            {
+                return particles[0];
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Runtime/FXSystem.cs
@@ -0,0 +1,33 @@
+﻿using EblanDev.ScenarioCore.GameFramework.Systems;
+using UnityEngine;
+
+namespace EblanDev.ScenarioCore.Systems.FXSystemUnit
+{
+    public class FXSystem : StaticSystemDated<FXData>
+    {
+        private static ParticleWorker particles;
+        
+        protected static Transform thisT;
+        
+        public override void Init()
+        {
+            particles = new ParticleWorker(transform);
+            thisT = transform;
+        }
+
+        public static void Particle(FXID ID, Vector3 pos, Vector3 dir, Transform parent = null)
+        {
+            var particle = particles.Create(DataStatic.GetParticle(ID));
+
+            particle.transform.position = pos;
+            particle.transform.rotation = Quaternion.LookRotation(-dir);
+
+            if (parent != null)
+            {
+                particle.ParentTo(parent, thisT);
+            }
+
+            particle.Play();
+        }
+    }
+}
